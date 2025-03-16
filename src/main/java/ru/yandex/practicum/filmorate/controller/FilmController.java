2619bb6cf616c4f91a411184ddbeef884bde588f
Ivan Baturo <baturo.ivan@bk.ru>
2025-03-16 12:53:52 +0300

    feat: loggs added

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -26,13 +26,16 @@ public class FilmController {
         validate(film);
         film.setId(getNextId());
         films.put(film.getId(), film);
+        log.info("Фильм {} добавлен в базу", film.getName());
         return film;
     }
 
     @PutMapping
-    public Film updateFilm (@RequestBody Film newFilm) {
-        if (newFilm.getId() == null){
-            throw new ValidationException("Id обновляемого фильма не указан");
+    public Film updateFilm(@RequestBody Film newFilm) {
+        if (newFilm.getId() == null) {
+            String info = "Id обновляемого фильма не указан";
+            log.error("Ошибка при обновлении фильма: {}", info);
+            throw new ValidationException(info);
         }
         validate(newFilm);
         Film oldFilm = films.get(newFilm.getId());
@@ -40,6 +43,7 @@ public class FilmController {
         oldFilm.setDescription(newFilm.getDescription());
         oldFilm.setReleaseDate(newFilm.getReleaseDate());
         oldFilm.setDuration(newFilm.getDuration());
+        log.info("Фильм {} обновлён успешно", oldFilm.getName());
         return oldFilm;
     }
 
@@ -48,7 +52,7 @@ public class FilmController {
                 film.getDescription().length() > 200 ||
                 film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28)) ||
                 film.getDuration().isNegative()) {
-
+            log.error("Данные о фильме {} не соответствуют необходимым", film.getName());
             throw new ValidationException("Некорректные данные фильма");
         }
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -23,45 +23,62 @@ public class UserController {
     }
 
     @PostMapping
-    public User postUser (@RequestBody User user) {
+    public User postUser(@RequestBody User user) {
         if (user.getEmail().isBlank() || !user.getEmail().contains("@")) {
-            throw new ValidationException("Email должен быть корректным и не пустым");
+            String info = "Email должен быть корректным и не пустым";
+            log.error("Ошибка при добавлении пользователя: {}", info);
+            throw new ValidationException(info);
         }
         if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
-            throw new ValidationException("Логин не должен быть пустым и не должен содержать пробелы");
+            String info = "Логин не должен быть пустым и не должен содержать пробелы";
+            log.error("Ошибка при добавлении пользователя: {}", info);
+            throw new ValidationException(info);
         }
         if (user.getBirthday().isAfter(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть в будущем");
+            String info = "Дата рождения не может быть в будущем";
+            log.error("Ошибка при добавлении пользователя: {}", info);
+            throw new ValidationException(info);
         }
         if (user.getName().isBlank()) {
+            log.warn("Предупреждение, поле name пусто, в него будет записан login = {}", user.getLogin());
             user.setName(user.getLogin());
         }
         user.setId(getNextId());
         users.put(user.getId(), user);
+        log.info("Пользователь {} успешно создан и добавлен в систему", user.getName());
         return user;
     }
 
     @PutMapping
-    public User updateUser (@RequestBody User newUser) {
+    public User updateUser(@RequestBody User newUser) {
         if (newUser.getId() == null) {
-            throw new ValidationException("Id должен быть указан");
+            String info = "Id должен быть указан";
+            log.error("Ошибка обновления пользователя: {}", info);
+            throw new ValidationException(info);
         }
         User oldUser = users.get(newUser.getId());
         if (oldUser == null) {
-            throw new ValidationException("Пользователь с таким ID не найден");
+            String info = "Пользователь не найден";
+            log.error("Ошибка обновления пользователя: {}", info);
+            throw new ValidationException(info);
         }
-        if (newUser.getEmail() != null){
+        if (newUser.getEmail() != null) {
+            log.info("Почта успешно обновлена");
             oldUser.setEmail(newUser.getEmail());
         }
-        if (newUser.getLogin() != null){
+        if (newUser.getLogin() != null) {
+            log.info("Логин успешно обновлен");
             oldUser.setLogin(newUser.getLogin());
         }
-        if (newUser.getName() == null || newUser.getName().isBlank()){
+        if (newUser.getName() == null || newUser.getName().isBlank()) {
+            log.info("Поле имени пусто, произведена замена на логин");
             oldUser.setName(newUser.getLogin());
         } else {
+            log.info("Имя успешно обновлено");
             oldUser.setName(newUser.getName());
         }
-        if (newUser.getBirthday() != null){
+        if (newUser.getBirthday() != null) {
+            log.info("Дата рождения успешно обновлена");
             oldUser.setBirthday(newUser.getBirthday());
         }
         return oldUser;
