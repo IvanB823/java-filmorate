2258360c4a2101c140064651b8bace201acf1dc6
Ivan Baturo <baturo.ivan@bk.ru>
2025-05-31 15:03:03 +0300

    feat: genre and rating classes added

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,33 +1,19 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 @Data
-@NoArgsConstructor
+@Builder
 public class User {
+
     private Long id;
     private String email;
     private String login;
     private String name;
     private LocalDate birthday;
     private Set<Long> friends;
-
-    public User(String email, String login, String name, LocalDate birthday) {
-        this.email = email;
-        this.login = login;
-        this.name = name;
-        this.birthday = birthday;
-    }
-
-    public User(Long id, String email, String login, String name, LocalDate birthday) {
-        this.id = id;
-        this.email = email;
-        this.login = login;
-        this.name = name;
-        this.birthday = birthday;
-    }
 }
