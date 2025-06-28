9234e82196243d4381b43810f310425e0d10a0d8
Ivan Baturo <baturo.ivan@bk.ru>
2025-06-28 17:01:16 +0300

    feat: RatingService added

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.film.GenreDbStorage;
